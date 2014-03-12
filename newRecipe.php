1fa6fff9dc8bf237e47e38fc754941daffba3380
c1ko <ciko@shackspace.de>
2014-03-12 13:22:53 +0100

    Added recipe phps

--- newRecipe.php
+++ newRecipe.php
@@ -4,37 +4,41 @@
 	<title>
 		New Recipe
 	</title>
-
-
 </head>
 
 <body>
-
-<?php
-
-	echo '
-		<form action="UtensilView.php" method="POST">
-		
-			<table>
-				<tr><td>Title: <br /> <input type="text" name="title" /> </td><td><img src="" name="img" width="150" height="150"</td></tr>
-				<tr><td>Description: </td><td><input type="text" name="description" /></td></tr>
-				<tr><td>Ingredients: </td><td><input type="text" name="title" /></td></tr>
-				<tr><td>Prep Time: </td><td><input type="text" name="prepTime" /></td></tr>
-				<tr><td>Difficulty: </td>
-				<td><select>
-						<option value="easy">Easy</option>
-						<option value="med">Medium</option>
-						<option value="hard">Hard</option>
-					</select>
-				</td></tr>
-				<tr><td>&nbsp</td><td><input type="submit" name="submit"</td></tr>
-
-			</table>
- 		</form>
-	
-	
-	';
-?>
+	<form action="recipeView.php" method="POST">
+		<table>
+			<tr>
+				<td>Description: </td>
+				<td><input type="text" name="recipeDescription" /></td>
+			</tr>
+			<tr>
+				<td>Cooking Steps:</td>
+				<td><textarea name="recipeSteps"></textarea></td>
+			</tr>
+			<tr>
+				<td>Ingredients:</td>
+				<td><textarea name="recipeIngredients"></textarea></td>
+			</tr>
+			<tr>
+				<td>Est. Duration:</td>
+				<td><input type="text" name="recipeDuration" /></td>
+			</tr>
+			<tr>
+				<td>Difficulty:</td>
+				<td><input type="text" name="recipeDifficulty" /></td>
+			</tr>
+			<tr>
+				<td>Needed Utilities </td>
+				<td><textarea name="recipeUtilities"></textarea></td>
+			</tr>
+			<tr>
+				<td></td>
+				<input type="submit" name="submit"</td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
 
